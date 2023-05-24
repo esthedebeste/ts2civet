42c364c694570047ceafc9c9522bd4f2349c7e13
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2023-05-24 22:19:57 +0200

    Expose public API TODO: type declarations

--- esbuild.js
+++ esbuild.js
@@ -6,13 +6,37 @@ import * as esbuild from "esbuild"
 const cjsCompat = `const{require,__filename,__dirname}=await(async()=>{const{createRequire:e}=await import("node:module"),{fileURLToPath:r}=await import("node:url");return{require:e(import.meta.url),__filename:r(import.meta.url),__dirname:r(new URL(".",import.meta.url))}})();`
 
 await esbuild.build({
-	entryPoints: ["src/cli.civet"],
-	outfile: "dist/cli.js",
+	entryPoints: ["src/transform.civet"],
+	outfile: "dist/transform.js",
 	bundle: true,
-	minify: false,
+	minify: true,
 	platform: "node",
 	format: "esm",
 	target: ["es2022"],
 	plugins: [civet({})],
+	banner: { js: cjsCompat },
+})
+
+await esbuild.build({
+	entryPoints: ["src/cli.civet"],
+	outfile: "dist/cli.js",
+	bundle: true,
+	minify: true,
+	platform: "node",
+	format: "esm",
+	target: ["es2022"],
+	plugins: [
+		civet({}),
+		{
+			// import from public api instead of bundling it into the cli again
+			name: "smaller-clijs",
+			setup(build) {
+				build.onResolve({ filter: /^.\/transform\.civet$/ }, () => ({
+					path: "./transform.js",
+					external: true,
+				}))
+			},
+		},
+	],
 	banner: { js: cjsCompat },
 })
